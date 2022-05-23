6812b6f27d96d17f675069e9796ff8f963ef1503
Layla Laptop <jenny7120@naver.com>
2022-05-23 22:09:12 +0900

    05-23

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = event => setToDo(event.target.value);
+  const onSubmit = event => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo]);
+    console.log(toDo);
+  };
   return (
     <div>
-      <h1> Welcome back!</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
